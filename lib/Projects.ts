e3cf7c877ca0f31627340e4332aa16b24166afed
Karem Mohamed <116497405+Karem-Mohamed-Dev@users.noreply.github.com>
2023-08-28 06:42:57 +0300

    Add CarHub Project

--- lib/Projects.ts
+++ lib/Projects.ts
@@ -1,12 +1,12 @@
 export const projectsData: project[] = [
     {
         id: 1,
-        title: 'Jopyfi',
-        desc: 'Jopyfi is a demo for jops website. with working search, filter and save jop button ',
-        img: 'https://iili.io/HQHeHil.png',
-        live_link: 'https://jopify-4ea7d.web.app/',
-        github_link: 'https://github.com/Karem-Mohamed-Dev/Jopyfi',
-        tech: ['React','JavaScript', 'Css', 'react-router', 'redux'],
+        title: 'CarHub',
+        desc: 'Car rental service',
+        img: 'https://iili.io/HycgLYu.png',
+        live_link: 'https://car-hub-karim.vercel.app/',
+        github_link: 'https://github.com/Karem-Mohamed-Dev/CarHub',
+        tech: ['NextJs','TypeScript', 'Tailwind'],
         reverce: false
     },
     {
@@ -18,5 +18,15 @@ export const projectsData: project[] = [
         github_link: 'https://github.com/Karem-Mohamed-Dev/Architecture-portfolio',
         tech: ['React','TypeScript', 'Sass', 'react-router'],
         reverce: true
+    },
+    {
+        id: 3,
+        title: 'Jopyfi',
+        desc: 'Jopyfi is a demo for jops website. with working search, filter and save jop button ',
+        img: 'https://iili.io/HQHeHil.png',
+        live_link: 'https://jopify-4ea7d.web.app/',
+        github_link: 'https://github.com/Karem-Mohamed-Dev/Jopyfi',
+        tech: ['React','JavaScript', 'Css', 'react-router', 'redux'],
+        reverce: false
     },
 ]
