90b652a5c2cbd731c50b7f27238e20d01e6dd196
Karem Mohamed <116497405+Karem-Mohamed-Dev@users.noreply.github.com>
2023-08-28 06:49:21 +0300

    Fix Projects title style

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -5,7 +5,7 @@ import SinglProject from './SinglProject'
 const Projects = () => {
   return (
     <section id='projects' className='min-h-screen py-28 px-5'>
-      <div className="container mx-auto flex flex-col items-center justify-around min-h-screen">
+      <div className="container mx-auto flex flex-col gap-16 items-center justify-around min-h-screen">
         <h2 className='text-4xl font-bold underline underline-offset-8 decoration-4 decoration-main'>Projects</h2>
         <div className="flex flex-col gap-y-5 py-10 lg:py-0">
           {projectsData.map(ele => (
